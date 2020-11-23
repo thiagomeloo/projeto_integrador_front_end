7a3ce1858e11ad17dac9d95d19b1af41a060a5c3
victorO13 <50842019+victorO13@users.noreply.github.com>
2020-11-23 20:10:53 -0300

    #3 Criação da Tela de Login

--- src/pages/loginCliente.js
+++ src/pages/loginCliente.js
@@ -2,7 +2,9 @@ import React from 'react'
 import {
   View,
   Text,
-  Button
+  Button,
+  Image,
+  TouchableOpacity
 } from 'react-native'
 
 //STYLES
@@ -12,9 +14,24 @@ import styleGlobal from '../styles/styleGlobal'
 export default function loginScreen({ navigation }) {
     return (
       <View style={styleGlobal.container}>
-        <Text>pagina login</Text>
-        <Button title="ir para pagina home"
-            onPress={() => navigation.navigate('main')}/>
+        <View style={styleGlobal.view}>
+          <Image
+        style={styleGlobal.icon}
+        source={require('../../assets/icon.png')}
+        />
+        </View>
+        
+        <View style={styleGlobal.view}>
+          <TouchableOpacity
+          style={styleGlobal.button}
+          onPress={() => navigation.navigate('inicial')}
+          >
+          <Text style={styleGlobal.button}>LOGIN COM GOOGLE</Text>
+          </TouchableOpacity> 
+        </View>
+        
+        <Text style={styleGlobal.txt}>CADASTRE-SE</Text>
+        
       </View>
     )
 }
