ad68a539c42ad2e8c90e18e7f437457abf92d73d
victorO13 <50842019+victorO13@users.noreply.github.com>
2020-12-21 23:52:45 -0300

    #27 edição de pratos

--- restaurante/src/pages/editPratos.js
+++ restaurante/src/pages/editPratos.js
@@ -1,4 +1,4 @@
-import React, {useState}from 'react'
+import React, { useState } from 'react'
 import {
   View,
   Text,
@@ -13,69 +13,81 @@ import styleCadastro from '../styles/styleCadastro'
 import DropDownPicker from 'react-native-dropdown-picker'
 import Textarea from 'react-native-textarea'
 
+import PageName from '../components/PageName'
 
 
 
+export default function inicialScreen({ route, navigation }) {
+  
+  const [nome, setNome] = useState(route.params.nome)
+  const [quant, setQuant] = useState(route.params.quantidade+'')
+  const [preco, setPreco] = useState(route.params.preco+'')
+  const [descricao, setDescricao] = useState(route.params.descricao)
+  const [categoria, setCategoria] = useState(route.params.categoria)
+  const [codigo, setCodigo] = useState(route.params.resturante_codigo)
+  const [email, setEmail] = useState(route.params.restaurante_email)
+  console.log(categoria)
+  
 
-export default function inicialScreen({ route,navigation }) {
-    
-    const item = route.params.item
-    const [valor, setValor] = useState('selecione') 
-    
-    return (
-      <View style={styleGlobal.container}>
-        <Image
-        style={[styleCadastro.icon2,styleGlobal.icon2,{right:95}]}
+  return (
+    <View style={styleGlobal.container}>
+      <Image
+        style={[styleCadastro.icon2, styleGlobal.icon2, { right: 95,top:10 }]}
         source={require('../../assets/icon2.png')}
+      />
+      <Text style={[styleGlobal.txtIcon, styleCadastro.txtIcon,{bottom:25}]}>EDITAR PRATO</Text>
+
+      <View style={styleCadastro.ViwInput}>
+        <Text style={styleCadastro.txt}>NOME:</Text>
+        <TextInput style={styleCadastro.imput} value={nome}
+          onChangeText={TextInput => setNome(TextInput)}
         />
-       <Text style={[styleGlobal.txtIcon,styleCadastro.txtIcon]}>EDITAR PRATOS</Text>
-        
-        <View style={styleCadastro.ViwInput}>
-    <Text style={styleCadastro.txt}>NOME:</Text>
-            <TextInput style={styleCadastro.imput}
-            />
 
-            <Text style={styleCadastro.txt}>CATEGORIA:</Text>
-            <DropDownPicker
-              items={[
-              { label: 'SELECIONE...',color:'red', value: 'selecione', hidden: true },
-              { label: 'ENTRADA', value: 'ENTRADA'},
-              { label: 'PRINCIPAL', value: 'PRINCIAP'},
-              { label: 'SOBREMESA', value: 'SOBREMESA'},
-              
-              ]}
-              defaultValue={valor}
-              style={styleCadastro.dropdown}
-              labelStyle={styleCadastro.dropdown_label}
-              containerStyle={styleCadastro.dropdown_Container}        
-              itemStyle={styleCadastro.dropdown_item}
-              dropDownStyle={styleCadastro.dropdown_style}
-              onChangeItem={item => setValor(item.value)}
-              zIndex={2}
-              />
+        <Text style={styleCadastro.txt}>CATEGORIA:</Text>
+        <DropDownPicker
+          items={[
+            { label: 'SELECIONE...', color: 'red', value: 'selecione', hidden: true },
+            { label: 'ENTRADA', value: 'entrada' },
+            { label: 'PRINCIPAL', value: 'principal' },
+            { label: 'SOBREMESA', value: 'sobremesa' },
+
+          ]}
+          defaultValue={categoria}
+          style={styleCadastro.dropdown}
+          labelStyle={styleCadastro.dropdown_label}
+          containerStyle={styleCadastro.dropdown_Container}
+          itemStyle={styleCadastro.dropdown_item}
+          dropDownStyle={styleCadastro.dropdown_style}
+          onChangeItem={item => setCategoria(item.value)}
+          zIndex={2}
+        />
         <Text style={styleCadastro.txt}>SERVE ATÉ:</Text>
-            <TextInput style={styleCadastro.imput}
-            
-            />
+        <TextInput style={styleCadastro.imput} value={quant}
+        onChangeText={TextInput => setQuant(TextInput)}
+        />
+        <Text style={styleCadastro.txt}>PREÇO:</Text>
+        <TextInput style={styleCadastro.imput} value={preco}
+          onChangeText={TextInput => setPreco(TextInput)}
+        />
+        <Text style={styleCadastro.txt}>DESCRIÇÃO:</Text>
+        <Textarea
+          containerStyle={styleCadastro.textareaContainer}
+          style={styleCadastro.textarea} value={descricao}
+          onChangeText={TextInput => setDescricao(TextInput)}
 
-            <Text style={styleCadastro.txt}>DESCRIÇÃO:</Text>
-            <Textarea
-                containerStyle={styleCadastro.textareaContainer}
-                style={styleCadastro.textarea}
-                
-            />
-       
+        />
+
         <View zIndex={1}>
           <TouchableOpacity
-          style={styleCadastro.Btn}
-          onPress={() => navigation.navigate('main')}
+            style={[styleCadastro.Btn,{marginBottom:25}]}
+            onPress={() => navigation.navigate('main')}
           >
-          <Text style={styleGlobal.button}>ATUALIZAR</Text>
-          </TouchableOpacity> 
+            <Text style={styleGlobal.button}>ATUALIZAR</Text>
+          </TouchableOpacity>
         </View>
-      
+
       </View>
     </View>
-    )
+  )
 }
 
--- restaurante/src/pages/pratos.js
+++ restaurante/src/pages/pratos.js
@@ -20,17 +20,17 @@ import PageName from '../components/PageName'
 
 export default function PratosScreen({ route,navigation }) {
 
-
+  
   const [listaPratos, setListaPratos] = useState(
     {
       'pratos': [
-        { "id": 1, "nome": "Sorvete de caju com cuscuz", "descricao": "descricao do prato, texto grande para testar a lista de pratos que existem.", 'categoria': 'sobremesa', 'quantidade': 2, "star": 2 },
-        { "id": 2, "nome": "Sorvete", "descricao": "descricao do prato", 'categoria': 'sobremesa', 'quantidade': 2, "star": 3 },
-        { "id": 3, "nome": "Sorvete", "descricao": "descricao do prato", 'categoria': 'sobremesa', 'quantidade': 2, "star": 4 },
-        { "id": 4, "nome": "Sorvete", "descricao": "descricao do prato", 'categoria': 'sobremesa', 'quantidade': 2, "star": 2 },
-        { "id": 5, "nome": "Sorvete", "descricao": "descricao do prato", 'categoria': 'sobremesa', 'quantidade': 2, "star": 3 },
-        { "id": 6, "nome": "Sorvete", "descricao": "descricao do prato", 'categoria': 'sobremesa', 'quantidade': 2, "star": 4 },
-        { "id": 7, "nome": "Sorvete", "descricao": "descricao do prato", 'categoria': 'sobremesa', 'quantidade': 2, "star": 4 }
+        { "id": 1, "nome": "Sorvete de caju com cuscuz", "descricao": "descricao do prato, texto grande para testar a lista de pratos que existem.", 'categoria': 'sobremesa', 'quantidade': 2,'preco':2.50, "star": 2 },
+        { "id": 2, "nome": "Sorvete", "descricao": "descricao do prato", 'categoria': 'sobremesa', 'quantidade': 2, 'preco':2.50, "star": 3 },
+        { "id": 3, "nome": "Sorvete", "descricao": "descricao do prato", 'categoria': 'sobremesa', 'quantidade': 2,'preco':2.50, "star": 4 },
+        { "id": 4, "nome": "Sorvete", "descricao": "descricao do prato", 'categoria': 'sobremesa', 'quantidade': 2,'preco':2.50, "star": 2 },
+        { "id": 5, "nome": "Sorvete", "descricao": "descricao do prato", 'categoria': 'sobremesa', 'quantidade': 2,'preco':2.50, "star": 3 },
+        { "id": 6, "nome": "Sorvete", "descricao": "descricao do prato", 'categoria': 'sobremesa', 'quantidade': 2, 'preco':2.50,"star": 4 },
+        { "id": 7, "nome": "Sorvete", "descricao": "descricao do prato", 'categoria': 'sobremesa', 'quantidade': 2, 'preco':2.50, "star": 4 }
       ]
     }
   )
@@ -56,8 +56,9 @@ export default function PratosScreen({ route,navigation }) {
             descricao={item.descricao}
             categoria={item.categoria}
             quantidade={item.quantidade}
+            preco={item.preco}
             star={item.star}
-            funcEdit={() => navigation.navigate('editPratos', { item })}
+            funcEdit={() => navigation.navigate('editPratos', item)}
             funcRemove={() => removePrato(item)}
           >
           </ItemListPratos>
