efc58e5150741d49547b98224a96aa79a3be89d7
thiagomeloo <4thiagomelo5@gmail.com>
2020-12-23 00:06:10 -0300

    correcao imput mascaras

--- restaurante/src/pages/finalizarCadastroRestaurante.js
+++ restaurante/src/pages/finalizarCadastroRestaurante.js
@@ -1,4 +1,4 @@
-import React, {useState}from 'react'
+import React, { useState } from 'react'
 import {
   View,
   Text,
@@ -7,6 +7,9 @@ import {
   TextInput,
 } from 'react-native'
 
+import { TextInputMask } from 'react-native-masked-text'
+
+
 //STYLES
 import styleGlobal from '../styles/styleGlobal'
 import styleCadastro from '../styles/styleCadastro'
@@ -14,115 +17,121 @@ import DropDownPicker from 'react-native-dropdown-picker'
 //MODEL
 import RestauranteModel from '../model/restauranteModel'
 
-export default function inicialScreen({route, navigation }) {
-  
-    const user = route.params.user
-    const [nomeCompleto, setNomeCompleto] = useState(user.name)
-    const [fantasia, setFantasia] = useState('')
-    const [email, setEmail] = useState(user.email)
-    const [cnpj, setCnpj] = useState('')
-    const [telefone, setTelefone] = useState('')
-    const [valorRegiao, setValorRegiao] = useState('selecione') //constante para o piker de região
-    
-    function prosseguir(){
-      let restaurante = new RestauranteModel(nomeCompleto,
-        email,cnpj,telefone,fantasia,
-        valorRegiao)
-        
-        navigation.navigate('finalizarCadastroDescricao',  restaurante )
-        
-    }
+export default function inicialScreen({ route, navigation }) {
 
-    return (
-      <View style={styleGlobal.container}>
-        <Image
+  const user = route.params.user
+  const [nomeCompleto, setNomeCompleto] = useState(user.name)
+  const [fantasia, setFantasia] = useState('')
+  const [email, setEmail] = useState(user.email)
+  const [cnpj, setCnpj] = useState('')
+  const [telefone, setTelefone] = useState('')
+  const [valorRegiao, setValorRegiao] = useState('selecione') //constante para o piker de região
+
+  function prosseguir() {
+    let restaurante = new RestauranteModel(nomeCompleto,
+      email, cnpj, telefone, fantasia,
+      valorRegiao)
+
+    navigation.navigate('finalizarCadastroDescricao', restaurante)
+
+  }
+
+  return (
+    <View style={styleGlobal.container}>
+      <Image
         style={styleCadastro.icon}
         source={require('../../assets/icon.png')}
+      />
+      <Text style={[styleGlobal.txtIcon, { bottom: 10 }]}>FINALIZAR CADASTRO</Text>
+
+      <View style={styleCadastro.ViwInput}>
+        <Text style={styleCadastro.txt}>NOME COMPLETO:</Text>
+        <TextInput style={styleCadastro.imput}
+          value={nomeCompleto} onChangeText={TextInput => setNomeCompleto(TextInput)}
         />
-       <Text style={[styleGlobal.txtIcon,{bottom:10}]}>FINALIZAR CADASTRO</Text>
-        
-        <View style={styleCadastro.ViwInput}>
-            <Text style={styleCadastro.txt}>NOME COMPLETO:</Text>
-            <TextInput style={styleCadastro.imput}
-              value={nomeCompleto} onChangeText={TextInput=>setNomeCompleto(TextInput)}
-            />
 
-            <Text style={styleCadastro.txt}>FANTASIA:</Text>
-            <TextInput style={styleCadastro.imput}
-              value={fantasia} onChangeText={TextInput=>setFantasia(TextInput)}
-            />
+        <Text style={styleCadastro.txt}>FANTASIA:</Text>
+        <TextInput style={styleCadastro.imput}
+          value={fantasia} onChangeText={TextInput => setFantasia(TextInput)}
+        />
 
-            <Text style={styleCadastro.txt}>E-MAIL:</Text>
-            <TextInput style={styleCadastro.imput}
-              value={email} onChangeText={TextInput=>setEmail(TextInput)}
-              editable={false}
-            />
+        <Text style={styleCadastro.txt}>E-MAIL:</Text>
+        <TextInput style={styleCadastro.imput}
+          value={email} onChangeText={TextInput => setEmail(TextInput)}
+          editable={false}
+        />
 
-            <Text style={styleCadastro.txt}>CNPJ:</Text>
-            <TextInput style={styleCadastro.imput}
-              value={cnpj} onChangeText={TextInput=>setCnpj(TextInput)}
-            />
+        <Text style={styleCadastro.txt}>CNPJ:</Text>
+        <TextInputMask
+          type={'cnpj'}
+          style={styleCadastro.imput}
+          value={cnpj}
+          onChangeText={cnpj => { setCnpj(cnpj) }}
+        />
 
-            <Text style={styleCadastro.txt}>TELEFONE:</Text>
-            <TextInput style={styleCadastro.imput}
-              value={telefone} onChangeText={TextInput=>setTelefone(TextInput)}
-            />
+        <Text style={styleCadastro.txt}>TELEFONE:</Text>
+        <TextInputMask
+            type={'cel-phone'}
+            value={telefone}
+            onChangeText={text => {setTelefone(text)}}
+            style={styleCadastro.imput}
+          />
 
-            <Text style={styleCadastro.txt}>REGIÃO:</Text>
-            <DropDownPicker
-              items={[
-              { label: 'SELECIONE...',color:'red', value: 'selecione', hidden: true },
-              { label: 'AC', value: 'AC'},
-              { label: 'AL', value: 'AL'},
-              { label: 'AP', value: 'AP'},
-              { label: 'AM', value: 'AM'},
-              { label: 'BA', value: 'BA'},
-              { label: 'CE', value: 'CE'},
-              { label: 'ES', value: 'ES'},
-              { label: 'GO', value: 'GO'},
-              { label: 'MA', value: 'MA'},
-              { label: 'MT', value: 'MT'},
-              { label: 'MS', value: 'MS'},
-              { label: 'MG', value: 'MG'},
-              { label: 'PA', value: 'PA'},
-              { label: 'PB', value: 'PB'},
-              { label: 'PR', value: 'PR'},
-              { label: 'PE', value: 'PE'},
-              { label: 'PI', value: 'PI'},
-              { label: 'RJ', value: 'RJ'},
-              { label: 'RN', value: 'RN'},
-              { label: 'RS', value: 'RS'},
-              { label: 'RO', value: 'RO'},
-              { label: 'RR', value: 'RR'},
-              { label: 'SC', value: 'SC'},
-              { label: 'SP', value: 'SP'},
-              { label: 'SE', value: 'SE'},
-              { label: 'TO', value: 'TO'},
-              { label: 'DF', value: 'DF'}
-              
-              ]}
-              defaultValue={valorRegiao}
-              style={styleCadastro.dropdown}
-              labelStyle={styleCadastro.dropdown_label}
-              containerStyle={styleCadastro.dropdown_Container}        
-              itemStyle={styleCadastro.dropdown_item}
-              dropDownStyle={styleCadastro.dropdown_style}
-              onChangeItem={item => setValorRegiao(item.value)}
-              zIndex={2}
-              />
-      
-       
+        <Text style={styleCadastro.txt}>REGIÃO:</Text>
+        <DropDownPicker
+          items={[
+            { label: 'SELECIONE...', color: 'red', value: 'selecione', hidden: true },
+            { label: 'AC', value: 'AC' },
+            { label: 'AL', value: 'AL' },
+            { label: 'AP', value: 'AP' },
+            { label: 'AM', value: 'AM' },
+            { label: 'BA', value: 'BA' },
+            { label: 'CE', value: 'CE' },
+            { label: 'ES', value: 'ES' },
+            { label: 'GO', value: 'GO' },
+            { label: 'MA', value: 'MA' },
+            { label: 'MT', value: 'MT' },
+            { label: 'MS', value: 'MS' },
+            { label: 'MG', value: 'MG' },
+            { label: 'PA', value: 'PA' },
+            { label: 'PB', value: 'PB' },
+            { label: 'PR', value: 'PR' },
+            { label: 'PE', value: 'PE' },
+            { label: 'PI', value: 'PI' },
+            { label: 'RJ', value: 'RJ' },
+            { label: 'RN', value: 'RN' },
+            { label: 'RS', value: 'RS' },
+            { label: 'RO', value: 'RO' },
+            { label: 'RR', value: 'RR' },
+            { label: 'SC', value: 'SC' },
+            { label: 'SP', value: 'SP' },
+            { label: 'SE', value: 'SE' },
+            { label: 'TO', value: 'TO' },
+            { label: 'DF', value: 'DF' }
+
+          ]}
+          defaultValue={valorRegiao}
+          style={styleCadastro.dropdown}
+          labelStyle={styleCadastro.dropdown_label}
+          containerStyle={styleCadastro.dropdown_Container}
+          itemStyle={styleCadastro.dropdown_item}
+          dropDownStyle={styleCadastro.dropdown_style}
+          onChangeItem={item => setValorRegiao(item.value)}
+          zIndex={2}
+        />
+
+
         <View zIndex={-1}>
           <TouchableOpacity zIndex={-1}
-          style={styleCadastro.Btn}
-          onPress={() => prosseguir()}
+            style={styleCadastro.Btn}
+            onPress={() => prosseguir()}
           >
-          <Text style={styleGlobal.button}>PROSSEGUIR</Text>
-          </TouchableOpacity> 
+            <Text style={styleGlobal.button}>PROSSEGUIR</Text>
+          </TouchableOpacity>
         </View>
-      
+
       </View>
     </View>
-    )
+  )
 }
 
